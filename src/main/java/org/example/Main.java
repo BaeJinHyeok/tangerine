8aed957b336c554237a3c6bc1e26e0afde102b14
jh <gumemoge@gmail.com>
2023-04-03 19:01:38 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
-
+import java.util.*;
+import java.util.Map.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int k = 6;
+        int l = 4;
+        int j = 2;
+        int[] tangerine  = {1, 3, 2, 5, 4, 5, 2, 3};
+        int[] tangerine2 = {1, 3, 2, 5, 4, 5, 2, 3};
+        int[] tangerine3 = {1, 1, 1, 1, 2, 2, 2, 3};
+
+        Solution sol = new Solution();
+
+        int answer = sol.Choice(k,tangerine);
+        int answer2 = sol.Choice(l,tangerine2);
+        int answer3 = sol.Choice(j,tangerine3);
+
+        System.out.println(answer);
+        System.out.println(answer2);
+        System.out.println(answer3);
     }
+
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,2 +1,32 @@
-package org.example;public class Solution {
+package org.example;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+
+    public int Choice(int k, int[] tangerine) {
+
+        int answer = 0;
+        Map<Integer, Integer> sizeMap = new HashMap<Integer, Integer>();
+
+        for (int size : tangerine){
+            sizeMap.put(size, sizeMap.getOrDefault(size,0) + 1 );
+        }
+
+        List<Integer> keyList = new ArrayList<>(sizeMap.keySet());
+
+        keyList.sort((o1,o2) -> sizeMap.get(o2) - sizeMap.get(o1));
+
+        int i = 0;
+        while( k > 0 ){
+            k -= sizeMap.get(keyList.get(i));
+            answer++;
+            i++;
+        }
+
+        return answer;
+    }
+
 }
